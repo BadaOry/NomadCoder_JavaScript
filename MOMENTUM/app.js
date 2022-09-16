c209353195b3bb5a61ab89b2d9b606e68b245dd5
bada.ory <bada.ory@gmail.com>
2022-09-16 23:25:39 +0900

    2022_0916_#4.0

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,7 +1,23 @@
-// [ #3.6 ~ 8 _ CSS in JS ]
+// const loginForm = document.querySelector("#login-form");
+// const loginInput = loginForm.querySelector("input");
+// const loginButton = loginForm.querySelector("button");
 
-// [ 실습 1 ] Event 를 listen 하는 두 번째 방법
-//            : by assigning an event listener to the oneventname property of this interface
+// * 위의 선언을 더 짧게 만들기 
+// : loginForm 을 지우고 바로 선언
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
-const h1 = document.querySelector(".hello h1");
+
+// [ 실습 01 ] click event 감지하기 + value 가져오기
+//             : 이름을 적고 클릭하면 console.log() 할 수 있고,
+//               이를 통해 이 value 를 저장할 수 있게 하는 밑작업
+
+function onLoginBtnClick() {
+    // console.dir(loginInput); // 이 Object 의 속성 중 value 를 찾는게 목표
+    console.log(loginInput.value);
+}
+
+loginButton.addEventListener("click", onLoginBtnClick);
+
+
 
